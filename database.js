f67cc46d94281c11ce5d9495a5bb840302ef3f86
Ghaith <g.kashlan@live.com>
2021-02-22 10:39:37 +0100

    håller på med contributer

--- database.js
+++ database.js
@@ -120,15 +120,16 @@ const updateQuestion = (async (data, sess) => {
         const dbCon = await dbPromise;
         const user = await dbCon.get('SELECT accounttype, email FROM users WHERE email=?', [sess.email]);
        // const userQuestion = await dbCon.get('SELECT userQuestion FROM questions WHERE userQuestion=?', [data.userQuestion]);
-        if (user.accounttype == 1) {
+        if (user.accounttype == 1 || user.accounttype == 3) {
             const update = await dbCon.run('UPDATE questions set category=?, title=?, question=?, duplicate=?, userQuestion=? WHERE id=?', [data.category, data.title, data.question, data.timeofquestion, data.duplicate, sess.email, data.id]);
             return update;
-        }
-        else if (user.email) {
-            const update = await dbCon.run('UPDATE questions set category=?, title=?, question=?,  duplicate=? ,userquestion=? WHERE id=?', [data.category, data.title, data.question, data.duplicate, sess.email, data.id]);
-            return update;
+         }
+        // else if (user.email) {
+        //     const update = await dbCon.run('UPDATE questions set category=?, title=?, question=?,  duplicate=? ,userquestion=? WHERE id=?', [data.category, data.title, data.question, data.duplicate, sess.email, data.id]);
+        //     return update;
 
-        } else {
+        // } 
+        else {
             throw new error('You have no question');
         }
 
